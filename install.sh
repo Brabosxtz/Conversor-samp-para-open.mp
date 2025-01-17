a786b8a61bc521c1afb7cd30d0cb7c5a6a2eabf6
Brabos <136845042+Brabosxtz@users.noreply.github.com>
2025-01-17 11:04:56 -0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-echo -e "\n\n\e[32mConversor SA-MP para Open.MP\n\e[0m"
+echo -e "\n\n\033[32mConversor SA-MP para Open.MP\n\e[0m"
 
-mkdir -p $PREFIX/bin
-
-curl -o $PREFIX/bin/convertomp -L https://raw.githubusercontent.com/Brabosxtz/Conversor-samp-para-open.mp/main/convertomp.py
-
-chmod +x $PREFIX/bin/convertomp
+yes | pkg update && pkg upgrade
+pkg install -y python
+pip install convertomp
 
 echo -e "\n\e[32mInstalação concluída com sucesso!\e[0m"
 echo -e "Você pode usar o comando executando:\n\e[33mconvertomp <arquivo> optional:[pasta_modulos]\e[0m"
